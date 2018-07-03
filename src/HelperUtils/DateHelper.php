872feee84b69821eb0fbe507ae5fab378efefd92
xqyjjq <walk_code@163.com>
2018-07-03 13:48:37 +0800

    封装当用工具包

---
+++ src/HelperUtils/DateHelper.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ *
+ * <b>DateHelper.php</b></br>
+ * <pre>时间辅助函数</pre>
+ *
+ * @author xqyjjq  walk_code@163.com
+ * @date 2018/6/11 11:06
+ *
+ */
+class DateHelper {
+
+	/**
+	 * @param $startDate
+	 * @param $endDate
+	 * @param $pattern
+	 *<pre>获取两个日期所有的时间</pre>
+	 * @author xqyjjq  walk_code@163.com
+	 * @date 2018/6/11
+	 * @throws
+	 * @return array
+	 */
+	public static function GetBetweenDates($startDate, $endDate, $pattern = 'Y-m-d') {
+		$begin = new DateTime($startDate);
+		$end   = new DateTime($endDate);
+
+		$interval  = new DateInterval('P1D');
+		$dateRange = new DatePeriod($begin, $interval, $end);
+
+		$range = [];
+		foreach ($dateRange as $date) {
+			$range[] = $date->format($pattern);
+		}
+
+		$range[] = $end->format($pattern);
+
+		return $range;
+	}
+
+
+	/**
+	 * @param $day
+	 *<pre>返回距离当前时间的xxx天后的剩余时间(秒)</pre>
+	 * @author xqyjjq  walk_code@163.com
+	 * @date 2018/6/11
+	 * @throws
+	 * @return int
+	 */
+	public static function GetRemainingSecondsAfterDays($day) {
+		$endDate   = strtotime('+' . $day . ' day');
+		$startDate = time();
+		return $endDate - $startDate;
+	}
+
+}
+
---
+++ src/HelperUtils/HttpHelper.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ *
+ * <b>HttpHelper.php</b></br>
+ * <pre>http请求辅助函数</pre>
+ *
+ * @author xqyjjq  walk_code@163.com
+ * @date 2018/6/11 12:28
+ *
+ */
+class  HttpHelper {
+
+	/**
+	 * @param $url
+	 * @param $param
+	 * @param $headers
+	 *<pre>发送Get请求</pre>
+	 * @author xqyjjq  walk_code@163.com
+	 * @date 2018/6/11
+	 * @throws
+	 * @return string
+	 */
+	public static function HttpGet($url, $params, $headers) {
+		$paramData = '';
+		foreach ($params as $k => $param) {
+			$paramData .= $k . '=' . $param . '&';
+		}
+		$paramData = rtrim($paramData, '&');
+
+		$curl = curl_init($url . '?' . $paramData);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		$responBody = curl_exec($curl);
+		curl_close($curl);
+
+		return $responBody;
+	}
+
+
+	/**
+	 * @param $url
+	 * @param $param
+	 * @param $headers
+	 *<pre>发送Get请求</pre>
+	 * @author xqyjjq  walk_code@163.com
+	 * @date 2018/6/11
+	 * @throws
+	 * @return string
+	 */
+	public static function HttpPost($url, $params, $headers, $type = 'x-www-form-urlencoded') {
+		$paramData = '';
+		foreach ($params as $k => $param) {
+			$paramData .= $k . '=' . $param . '&';
+		}
+		$paramData = rtrim($paramData, '&');
+
+		$curl = curl_init($url);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+		if ($type == 'json') {
+			curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($params));
+		} elseif ($type == 'xml') {
+
+		} else {
+			curl_setopt($curl, CURLOPT_POST, true);
+			curl_setopt($curl, CURLOPT_POSTFIELDS, $paramData);
+		}
+
+		$responBody = curl_exec($curl);
+		curl_close($curl);
+
+		return $responBody;
+	}
+
+
+}
